fa3cea8a7286f9ec90b05847432707a5b6a67677
CyberCeli <Aracelihair@gmail.com>
2021-06-23 14:39:40 -0600

    6/23

---
+++ app.js
@@ -0,0 +1,24 @@
+// Creating boilerplates
+
+// Functional buttons
+
+
+// Using Axios to extract makeup data
+
+
+// Nav bar/dropdown menu
+
+
+// Proper date from API
+
+
+// Render card with brand, name, description and image link
+
+
+// Styling with Flexbox
+
+
+// Using Axios to create search button
+
+
+// Remove previous search results
