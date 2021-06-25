ac942fc3d665cd6d86c5df0e07a9ee4860a6a2fc
CyberCeli <Aracelihair@gmail.com>
2021-06-25 14:50:18 -0600

    just one last code to fix

--- app.js
+++ app.js
@@ -1,42 +1,30 @@
+
 // Creating boilerplates
-
-// Proper date from API
-// get all products '
-
+// Proper data from API
+// get all products 
 // *** DROPDOWN MENU
 // Global Variables 
 const selectTag = document.querySelector('.selections')
 const form = document.querySelector('form')
-const dataContainer = document.querySelector('#container')
-
-
+const dataContainer = document.querySelector('.container')
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const optionValue = document.querySelector('select').value
+  // console.log(optionValue)
+  getProductList(optionValue)
+})
 // Make a request to dynamically create dropdown list from all the product catergories
-
-const getProductList = async () => {
+const getProductList = async (product) => {
   try {
-    const products = 'http://makeup-api.herokuapp.com/api/v1/products.json?product_type' 
+    const products = `http://makeup-api.herokuapp.com/api/v1/products.json?product_type=${product}`
     const response = await axios.get(products);
-    console.log(response.data);
-    const productList = response.data.message;
-    // console.log(productList)
-   
+    console.log(response);
   } catch (error) {
     console.error(error)
   }
 }
-getProductList()
-
-
 // Setup Option tag Value
-function setOptions(drop) {
-  console.log(drop)
-  list.forEach((products) =>{
-    let optionTag = document.createElement('option')
-    optionTag.textContent = product
-    optionTag.value = product
-    selectTag.append(optionTag)
-  })
-}
+
 
 // function names(listers){
 //   listers.forEach((names)=>{
@@ -47,40 +35,12 @@ function setOptions(drop) {
 //   });
 //   }
 // create option tags from the  product list 
-function getValue(e) {
-  e.preventDefault()
-  let optionValue = selectTag.getValue
-  console.log(optionValue)
-  getBreedImage(optionValue)
-  return optionValue
-}
+
 
 // Create Event handler to grab option value 
-form.addEventListener('submit', getValue)
 
 
 
-
-// Render card with brand, name, description and image link
-async function getProductImage(productValue) {
-  try {
-    let imageURL = `http://makeup-api.herokuapp.com/api/v1/products.json?product_type=blush`
-    const response = await axios.get(imageURL)
-  console.log(response.data.id)
-  // removeElement(container)
-  appendImage(response)
-} catch (error) {
-  console.error(error);
-}
-}
-
-// Image tag
-function appendImage(img) {
-  console.log(img)
-  const imageTag = document.createElement('img')
-  imageTag.setAttribute('src', img)
-  dataContainer.append(imageTag)
-}
 
 // Styling with Flexbox
 
