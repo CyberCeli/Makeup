3d4261d340f38376f08d01bd45c0a274166f95b1
CyberCeli <Aracelihair@gmail.com>
2021-06-28 07:59:54 -0600

    code

--- app.js
+++ app.js
@@ -7,47 +7,65 @@
 const selectTag = document.querySelector('.selections')
 const form = document.querySelector('form')
 const dataContainer = document.querySelector('.container')
+const products = document.querySelector('.products')
+
 form.addEventListener('submit', (e) => {
   e.preventDefault()
   const optionValue = document.querySelector('select').value
   // console.log(optionValue)
   getProductList(optionValue)
 })
+
+
 // Make a request to dynamically create dropdown list from all the product catergories
 const getProductList = async (product) => {
   try {
     const products = `http://makeup-api.herokuapp.com/api/v1/products.json?product_type=${product}`
     const response = await axios.get(products);
     console.log(response);
+    const productTypes = response.data
+    names(productTypes)
   } catch (error) {
     console.error(error)
   }
 }
-// Setup Option tag Value
 
+// Append name,description, brand, image and price
 
-// function names(listers){
-//   listers.forEach((names)=>{
-//       const options = document.createElement("option")
-//       options.textContent = names.name;
-//       options.value = names.id
-//       select.append(options);
-//   });
-//   }
-// create option tags from the  product list 
+function names(listers){
+  listers.forEach((names)=>{
+    const brand = document.createElement("h1")
+    const image = document.createElement("img")
+    const name = document.createElement("h3")
+    const price = document.createElement("p")
+    brand.textContent = names.brand
+    image.setAttribute("src", names.image_link)
+    name.textContent = names.name
+    price.textContent = names.price
+    brand.append(image, price, name)
+    products.append(brand)
+  });
+}
 
+function appendProductTypes(product) {
+  console.log(productTypes)
+  const imageTag = document.createElement('img')
+  imageTag.setAttribute('src', names.image_link)
+  dataContainer.append(imageTag)
+}
+  // Remove search history
 
-// Create Event handler to grab option value 
+  button.addEventListener('click', () => {
+    removeProducts()
+    getData()
+  })
+  function removeProducts() {
+    while (products.lastChild) {
+      productsremoveChild(products.lastChild)
+    }
+  }
 
+  // 
+  document.createElement("img");
 
-
-
-// Styling with Flexbox
-
-
-// Using Axios to create search button
-
-
-// Remove previous search results
-
-// Make new request to select new product category 
+  const img = document.createElement("img");
